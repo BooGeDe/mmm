15751f1786d320dff3733c20dbdc0eee89b8211c
BooGeDe <63404142+BooGeDe@users.noreply.github.com>
2023-01-27 20:19:51 +0200

    Base files , navbar about page

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,16 @@
+import React  from "react";
+
+
+const Contact= () => {
+  
+        return (
+        <div className="container">
+            <h2>
+                Contact us Page is under Construction
+            </h2>
+            
+        </div>
+        );
+};
+
+export {Contact};
